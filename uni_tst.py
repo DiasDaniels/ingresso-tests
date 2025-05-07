541a7e1abfa7e6f58aef3d6fdeb6df944978200f
Daniel Dias <cdias.jasper@gmail.com>
2025-05-06 21:48:29 -0300

    correção de códigos

--- uni_tst.py
+++ uni_tst.py
@@ -1,17 +1,17 @@
 def calcular_valor_tickets(qtd, idades):
-    if 0 < qtd <= 5:
-        valor = 0
-        for idade in idades[:qtd]:
-            if idade < 0 or idade > 100:
-                raise ValueError("Idade inválida")
-            if idade <= 12:
-                valor += 10
-            elif 13 <= idade <= 59:
-                valor += 30
-            else:
-                valor += 15
-        return f"Valor total: R$ {valor}"
-    elif qtd == 0:
-        raise ValueError("Nenhum ticket adicionado!")
-    else:
+    if qtd < 0 or qtd > 5:
         return "Quantidade máxima excedida!"
+    if qtd == 0:
+        return "Nenhum ticket adicionado!"
+    
+    valor = 0
+    for idade in idades[:qtd]:
+        if idade < 0 or idade > 100:
+            raise ValueError("Idade inválida")
+        if idade <= 12:
+            valor += 10
+        elif 13 <= idade <= 59:
+            valor += 30
+        else:
+            valor += 15
+    return f"Valor total: R$ {valor}"
